9f4b1989ff3dfa62e7ae91dedba7965769e8ffb2
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2022-12-22 15:01:10 +0600

    minimal ui setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,126 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="">
+    <main className="py-8">
+      <div className="container mx-auto px-4 grid min-h-screen place-items-center">
+        <div className="bg-slate-400 flex flex-col gap-4 text-2xl p-8">
+          <output className="h-28 border"></output>
+          <div className="flex flex-wrap gap-4">
+            <button
+              type="button"
+              className="flex items-center justify-center aspect-square p-2 bg-stone-200"
+              aria-label="one">
+              1
+            </button>
+            <button
+              type="button"
+              className="flex items-center justify-center aspect-square p-2 bg-stone-200"
+              aria-label="two">
+              2
+            </button>
+            <button
+              type="button"
+              className="flex items-center justify-center aspect-square p-2 bg-stone-200"
+              aria-label="three">
+              3
+            </button>
+            <button
+              type="button"
+              className="flex items-center justify-center aspect-square p-2 bg-stone-200"
+              aria-label="four">
+              4
+            </button>
+            <button
+              type="button"
+              className="flex items-center justify-center aspect-square p-2 bg-stone-200"
+              aria-label="five">
+              5
+            </button>
+            <button
+              type="button"
+              className="flex items-center justify-center aspect-square p-2 bg-stone-200"
+              aria-label="six">
+              6
+            </button>
+            <button
+              type="button"
+              className="flex items-center justify-center aspect-square p-2 bg-stone-200"
+              aria-label="seven">
+              7
+            </button>
+            <button
+              type="button"
+              className="flex items-center justify-center aspect-square p-2 bg-stone-200"
+              aria-label="eight">
+              8
+            </button>
+            <button
+              type="button"
+              className="flex items-center justify-center aspect-square p-2 bg-stone-200"
+              aria-label="nine">
+              9
+            </button>
+            <button
+              type="button"
+              className="flex items-center justify-center aspect-square p-2 bg-stone-200"
+              aria-label="zero">
+              0
+            </button>
+            <button
+              type="button"
+              className="flex items-center justify-center aspect-square p-2 bg-stone-200"
+              aria-label="point">
+              .
+            </button>
+          </div>
+          <div className="flex flex-wrap gap-4">
+            <button
+              type="button"
+              className="flex items-center justify-center aspect-square p-2 bg-stone-200"
+              aria-label="addition">
+              +
+            </button>
+            <button
+              type="button"
+              className="flex items-center justify-center aspect-square p-2 bg-stone-200"
+              aria-label="subtraction">
+              -
+            </button>
+            <button
+              type="button"
+              className="flex items-center justify-center aspect-square p-2 bg-stone-200"
+              aria-label="division">
+              /
+            </button>
+            <button
+              type="button"
+              className="flex items-center justify-center aspect-square p-2 bg-stone-200"
+              aria-label="multiply">
+              *
+            </button>
+            <button
+              type="button"
+              className="flex items-center justify-center aspect-square p-2 bg-stone-200"
+              aria-label="evaluate">
+              =
+            </button>
+            <button
+              type="button"
+              className="flex items-center justify-center aspect-square p-2 bg-stone-200"
+              aria-label="clear">
+              AC
+            </button>
+            <button
+              type="button"
+              className="flex items-center justify-center aspect-square p-2 bg-stone-200"
+              aria-label="delete">
+              Del
+            </button>
+          </div>
+        </div>
+      </div>
       <Toaster />
-    </div>
+    </main>
   );
 }
 
