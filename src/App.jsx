91feb3f8025b6abe595253e4906e0dd9fb65fada
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2022-12-22 14:33:44 +0600

    initialized

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Toaster } from "react-hot-toast";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="">
+      <Toaster />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/utilities/utilities.js
@@ -0,0 +1,4 @@
+export const APP_NAME = ``;
+export const APP_SERVER = `${
+  import.meta.env?.VITE_APP_SERVER || `http://localhost:5000`
+}`;
