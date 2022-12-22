c33602f8066d41c6a34cf0b3a8296def5258c8bc
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2022-12-22 17:12:18 +0600

    updated addDigit

--- src/reducers/calculatorReducer.jsx
+++ src/reducers/calculatorReducer.jsx
@@ -9,6 +9,12 @@ const calculatorActions = {
 const calculatorReducer = (state, { type, payload }) => {
   switch (type) {
     case calculatorActions?.addDigit:
+      if (state.currentOperand === "0" && payload.digit === "0") {
+        return state;
+      }
+      if (payload.digit === "." && state.currentOperand.includes(".")) {
+        return state;
+      }
       return {
         ...state,
         currentOperand: `${state?.currentOperand || ""}${payload?.digit}`
